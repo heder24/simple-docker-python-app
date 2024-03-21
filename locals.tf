ed1c9eb42fed5edf441e6232d6e3bc6b3e807806
heder redon <hederredon@gmail.com>
2024-03-20 22:48:13 -0400

    added terraform files and changed cidr range

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals {
   name   = "pro-python-app" #"ex-${basename(path.cwd)}"
   region = "eu-east-2"
 
-  vpc_cidr = "10.20.0/16"
+  vpc_cidr = "10.20.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
   tags = {
