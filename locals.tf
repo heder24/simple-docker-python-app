bb8e209906c4733aa8b4d34cfbdbebc3ebfbb62d
heder redon <hederredon@gmail.com>
2024-03-23 21:49:07 -0400

    Edited user data file

--- locals.tf
+++ locals.tf
@@ -9,49 +9,17 @@ locals {
     Name       = local.name
   }
   user_data                 = <<-EOT
-        #!/bin/bash
+    #!/bin/bash
 
-        # Uninstall unofficial versions of Docker and related packages
-        for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do 
-            sudo apt remove $pkg -y
-        done
+    # Update package lists and install Docker
+    sudo apt-get update -y
+    sudo apt-get install -y docker.io
 
-        # Update package lists
-        sudo apt-get update -y
+    # Pull the simple python Docker image
+    sudo docker pull heder24/simple-python
 
-        # Install necessary tools for adding Docker's Apt repository
-        sudo apt-get install ca-certificates curl gnupg -y
-
-        # Add Docker's official GPG key
-        sudo install -m 0755 -d /etc/apt/keyrings
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-        sudo chmod a+r /etc/apt/keyrings/docker.gpg 
-
-        # Add Docker's repository to Apt sources
-        echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-        $(. /etc/os-release && echo $VERSION_CODENAME) stable" | \
-        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-        # Update package lists again to include Docker's repository
-        sudo apt-get update -y
-
-        # Install Docker packages
-        sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-
-        # Start and enable Docker service
-        sudo service docker start
-        sudo service docker enable
-
-        # Add user 'cyber' to the 'docker' group to allow Docker commands without sudo
-        sudo usermod -a -G docker cyber
-
-        # Update Docker socket permissions
-        sudo chmod 666 /var/run/docker.sock
-
-        # Pull and run simple-python-app container
-        docker pull heder24/simple-python
-        docker run -d -p 80:8000 heder24/simple-python
+    # Run the Juice Shop container
+    sudo docker run  -p 80:8000 heder24/simple-python
         
   EOT
 }
